95d92dae449ab6229bc7e9a1ad4b34e67ee1de83
Chaimongkon Yutakom <chaimongkon.y@kbtg.tech>
2020-09-19 16:39:58 +0700

    HigherOrder Function

--- homatic/server.go
+++ homatic/server.go
@@ -4,24 +4,19 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
-type Pair struct {
-	DeviceID int64
-	UserID   int64
-}
-
-// git init, git add server.go go.mod, git commit -m "[Nong] init project"
-func main() {
+func main(){
 	fmt.Println("hello hometic : I'm Gopher!!")
+
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
+	r.Handle("/pair-device", PairDeviceHandler(createPairDevice)).Methods(http.MethodPost)
 
 	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 	fmt.Println("addr:", addr)
@@ -30,31 +25,49 @@ func main() {
 		Addr:    addr,
 		Handler: r,
 	}
+
 	log.Println("starting...")
 	log.Fatal(server.ListenAndServe())
 }
 
-func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
-	var p Pair
-	err := json.NewDecoder(r.Body).Decode(&p)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
-	defer r.Body.Close()
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+type Pair struct {
+	DeviceID int64
+	UserID int64
+}
 
+func PairDeviceHandler(createPairDevice CreatePairDeviceFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var p Pair
+		err := json.NewDecoder(r.Body).Decode(&p)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
+		defer r.Body.Close()
+		fmt.Printf("pair: %#v\n", p)
+
+
+		err = createPairDevice(p)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(err.Error())
+			return
+		}
+
+		w.Write([]byte(`{"status":"active"}`))
+	}
+}
+
+
+type CreatePairDeviceFunc func(p Pair) error
+
+func createPairDevice(p Pair) error {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
 
-	fmt.Printf("pair: %#v\n", p)
-	w.Write([]byte(`{"status":"active"}`))
+	_, err = db.Exec("INSERT INTO pairs VALUES (?,?);", p.DeviceID, p.UserID)
+	return err
 }
