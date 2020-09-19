9f2748efa2e2b29cef19b7a0cb84cfd4dabfaf40
Chaimongkon Yutakom <chaimongkon.y@kbtg.tech>
2020-09-19 14:26:23 +0700

    start heroku

--- homatic/server.go
+++ homatic/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -17,10 +18,10 @@ func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
 	r := mux.NewRouter()
 	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
-	
+
 	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 	fmt.Println("addr:", addr)
-	
+
 	server := http.Server{
 		Addr:    addr,
 		Handler: r,
